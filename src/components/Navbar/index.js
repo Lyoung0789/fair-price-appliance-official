3da4ff176155666207ac2d87afabd86617015e9e
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-13 19:06:56 -0400

    Creating the Nav Component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,8 @@
+//We can name this index. if we are calling the folder, react automitacally looks for the index.js file.
+import React from "react";
+
+const Navbar = () => {
+  return <></>;
+};
+
+export default Navbar;

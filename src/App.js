544e27cf48f2c3720b826ac9e8d5a029a94c27f7
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-13 19:22:06 -0400

    styling the navbar with css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,17 @@
 //We can name this index. if we are calling the folder, react automitacally looks for the index.js file.
 import React from "react";
+import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
 
 const Navbar = () => {
-  return <></>;
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">fairprice</NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;

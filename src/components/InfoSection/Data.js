d2b88d01605c14a700d7a7582c36298a16462e07
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-14 21:18:33 -0400

    Changing some data to reflect the different sections

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -18,37 +18,37 @@ export const homeObjOne = {
 };
 
 export const homeObjTwo = {
-  id: "about",
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: "About us",
-  headLine: "Sub Contractor of LG",
+  id: "services",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Services",
+  headLine: "What we do",
   description:
     "Based in sunny Orlando Fl.  but services between winter park, kissimmee, and other places around here",
-  buttonLabel: "Get Quote",
-  //imgStart switches the svg and the text
-  imgStart: true,
-  img: require("../../images/svg-2.svg"),
-  alt: "Appliance",
-  dark: true,
-  primary: true,
-  darkText: false,
-};
-
-export const homeObjThree = {
-  id: "about",
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: "About us",
-  headLine: "Sub Contractor of LG",
-  description:
-    "Based in sunny Orlando Fl.  but services between winter park, kissimmee, and other places around here",
-  buttonLabel: "Get Quote",
+  buttonLabel: "Contact Us",
   //imgStart switches the svg and the text
   imgStart: true,
   img: require("../../images/svg-3.svg"),
+  alt: "car",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "contact-us",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Contact Us",
+  headLine: "Call me",
+  description:
+    "Based in sunny Orlando Fl.  but services between winter park, kissimmee, and other places around here",
+  buttonLabel: "Get Quote",
+  //imgStart switches the svg and the text
+  imgStart: false,
+  img: require("../../images/svg-2.svg"),
   alt: "Appliance",
   dark: true,
   primary: true,

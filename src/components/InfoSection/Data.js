3e8abf285fe2cadaee4226d79e8216d2d63b5f36
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-14 20:31:55 -0400

    Updating info section's styling and wording.

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "About us",
+  headLine: "Sub Contractor of LG",
+  description:
+    "Based in sunny Orlando Fl.  but services between winter park, kissimmee, and other places around here",
+  buttonLabel: "Get Quote",
+  imgStart: true,
+  img: require("../../images/svg-5.svg"),
+  alt: "Appliance",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
